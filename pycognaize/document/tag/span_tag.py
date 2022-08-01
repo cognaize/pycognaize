b3944f8743ce8a9c9cb79b332d75344b5b46e6d2
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-01 17:14:38 +0400

    * Blank initialization function for span_tag

--- pycognaize/document/tag/span_tag.py
+++ pycognaize/document/tag/span_tag.py
@@ -1,3 +1,4 @@
+from pycognaize.document.spanning.line import Line
 from pycognaize.document.tag.tag import Tag
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -13,14 +14,16 @@ class SpanTag(Tag):
                  raw_value: str):
         super().__init__(left=left, right=right, top=top, bottom=bottom,
                          page=page)
+        self.line: Line
         self.raw_value = raw_value
         self._spacy_doc = None
 
-    @property
-    def spacy_doc(self):
-        if self._spacy_doc is None:
-            self._spacy_doc = self.__create_spacy_doc
-        return self._spacy_doc
+    def construct_from_raw(cls, raw: dict, page: 'Page') -> 'SpanTag':
+        ...
+
+    def construct_lines(self, metadata: dict) -> Line:
+        ...
+
 
     @module_not_found
     def __create_spacy_doc(self):
