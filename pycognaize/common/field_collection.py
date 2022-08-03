cbb7934513b1f0210de57f2f2fb97ed5cc964bf2
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-03 20:28:46 +0400

    * Added groups and groups_by_name functions for input and output fields

--- pycognaize/common/field_collection.py
+++ pycognaize/common/field_collection.py
@@ -6,7 +6,15 @@ from pycognaize.common.lazy_group_dict import LazyGroupDict
 class FieldCollection(OrderedDict):
     """Contains fields included in the document by also providing
     functionality to group fields by group_name and group_key"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def groups(self):
+    def groups(self) -> dict:
         """Return Groups"""
-        LazyGroupDict(self.__root)
+        return LazyGroupDict(self).groups
+
+    def groups_by_name(self, name: str) -> dict:
+        """Returns fields that are contained in group with the
+        given group name"""
+        return LazyGroupDict(self)[name]
+
