a45b0c6979447129beccd0c21d0be7ea529a99f5
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-03 18:36:11 +0400

    * Field Grouping in LazyDict

---
+++ pycognaize/common/lazy_group_dict.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+from collections.abc import Mapping
+
+
+class LazyGroupDict(Mapping):
+    """Contains groups included in the document"""
+
+    def __init__(self, document_x: dict, document_y: dict):
+        self._groups = None
+        self.create_groups(document_x, document_y)
+
+    def create_groups(self, document_x: dict, document_y: dict):
+        """Add fields to group """
+        groups = defaultdict(dict)
+        group_names = set()
+
+        input_output_fields = [document_x, document_y]
+        
+        for document_fields in input_output_fields:
+            for field_name in document_fields.keys():
+                group_names.update(
+                    [item.group_name for item in document_fields[field_name]
+                     if item.group_name])
+
+            for group_name in group_names:
+                groups[group_name] = defaultdict(list)
+                for field_name in document_fields.keys():
+                    for item in document_fields[field_name]:
+                        if item.group_name == group_name:
+                            groups[group_name][item.group_key].append(item)
+
+        self._groups = groups
+
+    def __getitem__(self, group_name) -> dict:
+        """The Document object, retrieved from provided path
+        Note: Path can be both local and remote
+        """
+        # return self._groups[group_name]
+        return self._groups[group_name]
+
+    def __iter__(self):
+        ...
+
+    def __len__(self):
+        ...
