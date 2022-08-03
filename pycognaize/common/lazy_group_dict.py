ea8efe69f97e94cac68c2f8a265d91accb9d925e
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-03 20:29:14 +0400

    * Updated LazyGroupDict to work with either output field or input field

--- pycognaize/common/lazy_group_dict.py
+++ pycognaize/common/lazy_group_dict.py
@@ -5,22 +5,23 @@ from collections.abc import Mapping
 class LazyGroupDict(Mapping):
     """Contains groups included in the document"""
 
-    def __init__(self, document_x: dict, document_y: dict):
+    def __init__(self, document_x: dict):
         self._groups = None
-        self.create_groups(document_x, document_y)
+        self.create_groups(document_x)
 
-    def create_groups(self, document_x: dict, document_y: dict):
+    @property
+    def groups(self) -> dict:
+        return self._groups
+
+    def create_groups(self, document_fields: dict):
         """Add fields to group """
         groups = defaultdict(dict)
         group_names = set()
 
-        input_output_fields = [document_x, document_y]
-        
-        for document_fields in input_output_fields:
-            for field_name in document_fields.keys():
-                group_names.update(
-                    [item.group_name for item in document_fields[field_name]
-                     if item.group_name])
+        for field_name in document_fields.keys():
+            group_names.update(
+                [item.group_name for item in document_fields[field_name]
+                 if item.group_name])
 
             for group_name in group_names:
                 groups[group_name] = defaultdict(list)
