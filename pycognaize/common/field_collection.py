eeda08cdb9e9d95fe996db1c0ea3db63f83d2a3c
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-09 17:39:00 +0400

    * Updated Lazygroup creation in field_collection.py

--- pycognaize/common/field_collection.py
+++ pycognaize/common/field_collection.py
@@ -8,10 +8,13 @@ class FieldCollection(OrderedDict):
     functionality to group fields by group_name and group_key"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._groups = None
 
     def groups(self) -> dict:
         """Return Groups"""
-        return LazyGroupDict(self).groups
+        if self._groups is None:
+            self._groups = LazyGroupDict(self).groups
+        return self._groups
 
     def groups_by_name(self, name: str) -> dict:
         """Returns fields that are contained in group with the
