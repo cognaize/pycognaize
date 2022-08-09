de90d49d2b1cf9744ca37af85dff9289392a460e
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-09 18:34:20 +0400

    * Updated field collection LazyDict creation

--- pycognaize/common/lazy_group_dict.py
+++ pycognaize/common/lazy_group_dict.py
@@ -5,15 +5,17 @@ from collections.abc import Mapping
 class LazyGroupDict(Mapping):
     """Contains groups included in the document"""
 
-    def __init__(self, document_x: dict):
+    def __init__(self, document_fields: dict):
+        self._document_fields = document_fields
         self._groups = None
-        self.create_groups(document_x)
 
     @property
     def groups(self) -> dict:
+        if self._groups is None:
+            self.__create_groups(self._document_fields)
         return self._groups
 
-    def create_groups(self, document_fields: dict):
+    def __create_groups(self, document_fields: dict):
         """Add fields to group """
         groups = defaultdict(dict)
         group_names = set()
@@ -23,12 +25,12 @@ class LazyGroupDict(Mapping):
                 [item.group_name for item in document_fields[field_name]
                  if item.group_name])
 
-            for group_name in group_names:
-                groups[group_name] = defaultdict(list)
-                for field_name in document_fields.keys():
-                    for item in document_fields[field_name]:
-                        if item.group_name == group_name:
-                            groups[group_name][item.group_key].append(item)
+        for group_name in group_names:
+            groups[group_name] = defaultdict(list)
+            for field_name in document_fields.keys():
+                for item in document_fields[field_name]:
+                    if item.group_name == group_name:
+                        groups[group_name][item.group_key].append(item)
 
         self._groups = groups
 
@@ -37,7 +39,7 @@ class LazyGroupDict(Mapping):
         Note: Path can be both local and remote
         """
         # return self._groups[group_name]
-        return self._groups[group_name]
+        return self.groups[group_name]
 
     def __iter__(self):
         ...
