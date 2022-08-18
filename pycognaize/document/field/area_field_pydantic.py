9a10a6fcb25e9038c2fedc3f67fdc948354dc405
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-18 18:20:47 +0400

    * Field and AreaField converted to pydantic * pydantic added to requirements.txt

---
+++ pycognaize/document/field/area_field_pydantic.py
@@ -0,0 +1,70 @@
+import logging
+from typing import Dict, List
+
+from pydantic import validator, root_validator
+
+from pycognaize.common.enums import (
+    IqDocumentKeysEnum,
+    IqTagKeyEnum,
+    ID,
+    IqFieldKeyEnum,
+    IqDataTypesEnum
+)
+from pycognaize.document import Page
+from pycognaize.document.field.field_pydantic import Field
+
+
+class AreaField(Field):
+    """Representing AreaField in pycognaize"""
+
+    @root_validator()
+    def validate_value(cls, values):
+        """Validate and create value from tags"""
+        tags = values['tags']
+        value = values['value']
+        if tags:
+            value = '; '.join([i.raw_value for i in tags])
+        elif isinstance(value, str):
+            value = value
+        else:
+            logging.warning(
+                f"Expected value of type str, received {type(values)},"
+                f" setting value to empty string")
+            value = ''
+        values['value'] = value
+        return values
+
+    @classmethod
+    def construct_from_raw(cls, raw: dict,
+                           pages: Dict[int, Page]) -> 'AreaField':
+        """Create AreaField object from dictionary"""
+        tag_dicts: List[dict] = raw[IqDocumentKeysEnum.tags.value]
+        tags = []
+        for i in tag_dicts:
+            try:
+                tags.append(cls.tag_class.construct_from_raw(
+                    raw=i, page=pages[i['page']]))
+            except Exception as e:
+                logging.debug(f"Failed creating tag for field {raw[ID]}: {e}")
+        return cls(name=raw[IqDocumentKeysEnum.name.value],
+                   value=raw[IqTagKeyEnum.value.value],
+                   tags=tags,
+                   field_id=str(raw[ID]),
+                   group_key=raw.get(IqFieldKeyEnum.group_key.value, ''),
+                   group_name=raw.get(IqFieldKeyEnum.group.value, '')
+                   )
+
+    def to_dict(self) -> dict:
+        """Converts AreaField object to dictionary"""
+        field_dict = super().to_dict()
+        field_dict[IqFieldKeyEnum.value.value] = self.value
+        field_dict[ID] = self.field_id
+        field_dict[IqFieldKeyEnum.data_type.value] = IqDataTypesEnum.area.value
+        return field_dict
+
+    def __repr__(self):
+        return (f"<{self.__class__.__name__}: {self.name}:"
+                f" {self.value}>")
+
+    def __str__(self):
+        return f"{self.value}"
---
+++ pycognaize/document/field/field_pydantic.py
@@ -0,0 +1,63 @@
+import sys
+from typing import Optional, List, Dict
+
+from pydantic import BaseModel, validator
+
+from pycognaize.common.decorators import soon_be_deprecated
+from pycognaize.common.enums import IqFieldKeyEnum
+from pycognaize.document import Page
+from pycognaize.document.tag import ExtractionTag
+
+
+class Field(BaseModel):
+    """Base class for all Field types in pycognaize"""
+
+    class Config:
+        """Configuration for Field Pydantic model"""
+        arbitrary_types_allowed = True
+        validate_assignment = True
+        if sys.version_info < (3, 9):
+            kw_only = False
+
+    name: str
+    value: str = ''
+    tags: Optional[List[ExtractionTag]] = None
+    field_id: Optional[str] = None
+    group_key: Optional[str] = None
+    group_name: Optional[str] = None
+    confidence: Optional[float] = -1.0
+
+    @validator('tags')
+    def validate_tags(cls, v):
+        if v is None:
+            return []
+        return v
+
+    @classmethod
+    @soon_be_deprecated(version='1.2.0')
+    def construct_from_raw(cls, raw: dict, pages: Dict[int, Page]) -> 'Field':
+        """Use raw dictionary in order to recreate the Field python object"""
+        pass
+
+    @soon_be_deprecated(version='1.2.0')
+    def to_dict(self) -> dict:
+        """Return a dictionary representing the field object"""
+        field_dict = dict()
+        field_dict[IqFieldKeyEnum.name.value] = self.name
+        if self.group_key:
+            field_dict[IqFieldKeyEnum.group_key.value] = self.group_key
+        field_dict[IqFieldKeyEnum.tags.value] = [
+            i.to_dict() for i in self.tags]
+        return field_dict
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: {self.name}>"
+
+    def __str__(self):
+        return f"{self.tags}"
+
+if __name__ == '__main__':
+    a = Field(name='a')
+    a.name = 123
+    print(type(a.name))
+    a.to_dict()
