92890465644e7cf8f41da22af22fa759c0ea13e0
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-08-22 22:53:10 +0400

    * Updated test_text_field to work with pydatnic text_field

---
+++ pycognaize/document/field/text_field.py
@@ -0,0 +1,71 @@
+import logging
+from typing import Dict, List, Type
+
+from pydantic import root_validator
+
+from pycognaize.common.enums import (
+    IqDocumentKeysEnum,
+    ID,
+    IqFieldKeyEnum,
+    IqDataTypesEnum, IqTagKeyEnum
+)
+from pycognaize.document import Page
+from pycognaize.document.field.field import Field
+from pycognaize.document.tag import ExtractionTag
+
+
+class TextField(Field):
+    """Representing AreaField in pycognaize"""
+
+    _tag_class: Type[ExtractionTag] = ExtractionTag
+
+    @root_validator()
+    def validate_value(cls, values):
+        """Validate and create value from tags"""
+        tags = values['tags']
+        value = values['value']
+        # print(tags, values)
+        if tags:
+            value = '; '.join([i.raw_value for i in tags]) \
+                if tags else value
+        else:
+            logging.warning(
+                f"Expected tags array, received {tags},"
+                f" setting value to empty string")
+        values['value'] = value
+        return values
+
+    @classmethod
+    def construct_from_raw(cls, raw: dict, pages: Dict[int, Page]) -> 'TextField':
+        """Create TextField object from dictionary"""
+        tag_dicts: List[dict] = raw[IqDocumentKeysEnum.tags.value]
+        tags = []
+        for i in tag_dicts:
+            try:
+                tags.append(cls._tag_class.construct_from_raw(
+                    raw=i, page=pages[i['page']]))
+            except Exception as e:
+                logging.debug(f"Failed creating tag for field {raw[ID]}: {e}")
+        # print(tag_dicts)
+        return cls(name=raw[IqDocumentKeysEnum.name.value],
+                   value=raw[IqTagKeyEnum.value.value],
+                   tags=tags,
+                   field_id=str(raw[ID]),
+                   group_key=raw.get(IqFieldKeyEnum.group_key.value, ''),
+                   group_name=raw.get(IqFieldKeyEnum.group.value, '')
+                   )
+
+    def to_dict(self) -> dict:
+        """Converts TextField object to dictionary"""
+        field_dict = super().to_dict()
+        field_dict[ID] = self.field_id
+        field_dict[IqFieldKeyEnum.value.value] = self.value
+        field_dict[IqFieldKeyEnum.data_type.value] = IqDataTypesEnum.text.value
+        return field_dict
+
+    def __repr__(self):
+        return (f"<{self.__class__.__name__}: {self.name}:"
+                f" {'|'.join([i.raw_value for i in self.tags])}>")
+
+    def __str__(self):
+        return f"{'|'.join([i.raw_value for i in self.tags])}"
