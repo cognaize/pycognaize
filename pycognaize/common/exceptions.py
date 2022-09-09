1058ad8b3e900b69bb786681c8cee229ee50713a
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-09-09 13:25:20 +0400

    * created basic login functionality * created custom exception for AWS connection

--- pycognaize/common/exceptions.py
+++ pycognaize/common/exceptions.py
@@ -1,2 +1,8 @@
 class DrawOnShellException(Exception):
     pass
+
+
+class AWS_connection_exception(Exception):
+    def __init__(self):
+        message = "AWS connection failed"
+        super().__init__(message)
--- pycognaize/login.py
+++ pycognaize/login.py
@@ -1,22 +1,55 @@
+import json
+import logging
+import requests
+import tempfile
+
+from pycognaize.common.exceptions import AWS_connection_exception
+
+
 class login:
 
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
+        self._login(username, password)
 
+    def _login(self, username: str = '', password: str = ''):
+        # url = CFG.host + CFG.API_ENDPOINT
+        url = "http://0.0.0.0:5002/api/v1/integration/storage/token"
 
-    def login(self):
-        # Try logging in and return error if cannot
-        ...
+        authentication = {'username': username,
+                          'password': password}
 
-    def _create_aws_config_file(self):
+        try:
+            user_credentials_response = requests.post(url, json=authentication)
+        except requests.exceptions.ConnectionError:
+            logging.info(f'Failed connecting to url: {url}')
+            raise AWS_connection_exception
+        except requests.exceptions.Timeout:
+            logging.info(f'Connection timed out: {url}')
+            raise AWS_connection_exception
+
+        user_credentials = user_credentials_response.json()
+        if user_credentials_response.status_code == 200:
+            credentials_file = self._create_aws_config_file()
+            self._write_aws_config_file(user_credentials, credentials_file)
+        elif user_credentials_response.status_code == 403:
+            logging.info(f'You are not allowed to download data')
+            raise AWS_connection_exception
+        elif user_credentials_response.status_code == 401:
+            logging.info(f'Invalid Username or Password')
+            raise AWS_connection_exception
+        else:
+            logging.info(f'Failed to login: {user_credentials}')
+            raise AWS_connection_exception
+
+    @staticmethod
+    def _create_aws_config_file() -> tempfile.NamedTemporaryFile:
         # Creates AWS config file in /tmp directory
-        ...
+        temp = tempfile.NamedTemporaryFile(prefix='cognaize_aws_access', suffix='.json', delete=False)
+        return temp
 
-    def _remove_aws_config(self):
-        # Removes AWS config file from /tmp directory
-        ...
-
-    def __del__(self):
-        # Removes AWS config file from /tmp directory
-       self._remove_aws_config()
+    @staticmethod
+    def _write_aws_config_file(credentials, file) -> str:
+        """Writes AWS credentials to a file and returns files name"""
+        json.dump(credentials, file)
+        file.flush()
+        return file.name
