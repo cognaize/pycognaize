6c90bce9a86ee92ac335893448eb39148fe30e59
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2022-09-12 13:12:01 +0400

    * Updated snapshot to read credentials from file created with cognaize login

--- pycognaize/document/snapshot.py
+++ pycognaize/document/snapshot.py
@@ -1,6 +1,7 @@
+import json
 import os
 from typing import Mapping
-
+from glob import glob
 
 from pycognaize.common.enums import EnvConfigEnum
 from pycognaize.common.lazy_dict import LazyDocumentDict
@@ -23,9 +24,15 @@ class Snapshot:
     @classmethod
     def _snapshot_path(cls) -> str:
         """Identify and return the snapshot path"""
-        snapshot_dir = os.environ[EnvConfigEnum.SNAPSHOT_PATH.value]
-        snapshot_id = os.environ[EnvConfigEnum.SNAPSHOT_ID.value]
-        snapshot_path = os.path.join(snapshot_dir, snapshot_id)
+        AWS_credentials_files = glob("/tmp/cognaize_aws_access_*.json")
+        if AWS_credentials_files:
+            credentials_file = json.load(open(AWS_credentials_files[0]))
+            AWS_credentials = credentials_file['credentials']
+            snapshot_path = credentials_file['path']
+        else:
+            snapshot_dir = os.environ[EnvConfigEnum.SNAPSHOT_PATH.value]
+            snapshot_id = os.environ[EnvConfigEnum.SNAPSHOT_ID.value]
+            snapshot_path = os.path.join(snapshot_dir, snapshot_id)
         return snapshot_path
 
     @classmethod
