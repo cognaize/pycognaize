0647072b69f5759d176ac74dd25e721cd19a438b
cognaize <astghik.chobanyan@cognaize.com>
2022-10-11 14:17:37 +0400

    minor change

--- pycognaize/common/table_utils.py
+++ pycognaize/common/table_utils.py
@@ -14,7 +14,7 @@ def filter_out_invalid_tables(tables):
     return valid_tables
 
 
-def sort_table_horizontally(tables: List['Field'], threshold: float):
+def sort_table_horizontally(tables, threshold: float):
     groups = []
     for table in tables:
         if not groups:
@@ -42,14 +42,14 @@ def sort_table_horizontally(tables: List['Field'], threshold: float):
     return sorted_tables
 
 
-def assign_indices_to_tables(tables: List['Field'], threshold: float = 0.4):
+def assign_indices_to_tables(tables, threshold: float = 0.4):
     tables_dict = {}
     valid_tables = filter_out_invalid_tables(tables)
     sorted_tables = sorted(valid_tables,
                            key=lambda x: x.tags[0].page.page_number)
     grouped_tables = {page: list(table) for page, table in
                       groupby(sorted_tables,
-                                        key=lambda x: x.tags[0].page.page_number)}
+                              key=lambda x: x.tags[0].page.page_number)}
     for page, page_tables in grouped_tables.items():
         sorted_page_tables = sorted(page_tables, key=lambda x: x.tags[0].left)
         final_ordered_tables = sort_table_horizontally(sorted_page_tables,
