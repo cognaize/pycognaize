8b9c6bcbe075363a58abed833dd40e4e826d6b03
hovhannes-zohrabyan <john.zohrabyan@gmail.com>
2023-01-27 16:49:50 +0400

    Update:
    * Added typing for comfidence

--- pycognaize/common/confidence.py
+++ pycognaize/common/confidence.py
@@ -1,3 +1,7 @@
+from collections.abc import dict_keys
+from typing import Union
+
+
 class Confidence:
     """
     This class is used by tag object to store confidence values
@@ -16,38 +20,39 @@ class Confidence:
         else:
             self.confidences = {}
 
-    def add_class(self, class_name, confidence):
+    def add_class(self, class_name: str,
+                  confidence: Union[float, int]) -> None:
         """Add new class for confidence scores
         :param class_name: name of the class
         :param confidence: confidence score for the class
         """
         self.confidences[class_name] = confidence
 
-    def number_of_classes(self):
+    def number_of_classes(self) -> int:
         """Returns number of classes"""
         return len(self.confidences)
 
-    def class_names(self):
+    def class_names(self) -> dict_keys:
         """Returns class names"""
         return self.confidences.keys()
 
-    def get_confidence(self):
+    def get_confidence(self) -> dict:
         """Returns confidence scores if finalized"""
         if not self._is_finalized:
             raise ValueError('Confidence scores have to be finalized.')
         return self.confidences
 
     @property
-    def is_finalized(self):
+    def is_finalized(self) -> bool:
         return self._is_finalized
 
-    def finalize(self):
+    def finalize(self) -> None:
         """Finalize confidence scores by checking if all scores sum to 1"""
         if not sum(self.confidences.values()) == 1:
             raise ValueError('Confidence scores have to sum up to 1.')
         self._is_finalized = True
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> Union[float, int]:
         if not self._is_finalized:
             raise ValueError('Confidence scores have to be finalized.')
         if item in self.confidences:
@@ -55,5 +60,5 @@ class Confidence:
         else:
             raise KeyError(f'Class {item} not found in confidence scores.')
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key, value) -> None:
         self.confidences[key] = value
