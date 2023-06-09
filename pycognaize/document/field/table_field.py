9a6c641a326273afaee96b5650e8f8d128cc8f2d
Rafo_Antonyan <rafoantonyan99@gmail.com>
2023-06-09 15:31:44 +0400

    Code:
    amended __repr__ and __str__ methods. Overrode the same thing which was already inherited.

--- pycognaize/document/field/table_field.py
+++ pycognaize/document/field/table_field.py
@@ -126,8 +126,6 @@ class TableField(Field):
         field_dict[IqFieldKeyEnum.value.value] = ''
         return field_dict
 
-    def __repr__(self):
-        return f"<{self.__class__.__name__}: {self.name}>"
 
     def __str__(self):
-        return self.__repr__()
+        return f"<{self.__class__.__name__}: {self.name}>"
