3d4ef578a36879dfca4ceac81c97f3089d7c51d6
IngaMelikyan <ingakmelikyan@gmail.com>
2024-02-07 13:03:18 +0400

    Change variable name in test_create_lines Add changes to test_get_ocr_formatted

--- pycognaize/tests/test_document/test_page.py
+++ pycognaize/tests/test_document/test_page.py
@@ -121,8 +121,8 @@ class TestPage(unittest.TestCase):
         self.assertEqual(self.page6.lines[1][0]['ocr_text'], 'scope')
 
     def test_create_lines(self):
-        no_tags = self.page6._create_lines(return_tags=True)
-        self.assertIsInstance(no_tags[0][0], pycognaize.document.tag.extraction_tag.ExtractionTag)
+        return_tags = self.page6._create_lines(return_tags=True)
+        self.assertIsInstance(return_tags[0][0], pycognaize.document.tag.extraction_tag.ExtractionTag)
 
     def test_search_text(self):
         area_dict = {'top': 1000, 'bottom': 1500, 'left': 200, 'right': 500}
@@ -178,7 +178,8 @@ class TestPage(unittest.TestCase):
     def test_get_ocr_formatted(self):
         formatted_ocr = self.page6.get_ocr_formatted()
         formatted_ocr_stuck = self.page6.get_ocr_formatted(stick_coords=True)
-
+        return_tags = self.page6.get_ocr_formatted(return_tags=True)
+        self.assertIsInstance(return_tags[0], pycognaize.document.tag.extraction_tag.ExtractionTag)
         self.assertEqual(formatted_ocr['words'][86]['ocr_text'], '188.')
         self.assertEqual(formatted_ocr['words'][86]['bottom'], 375)
         self.assertEqual(formatted_ocr['words'][86]['top'], 341)
